a572bfa60c6cdc6f7eef7583578169bdd6c0fc93
Вячеслав Менюхов <menyukhov@bk.ru>
2023-06-27 19:48:08 +0300

    Fix post_form.html and blogicum/urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -13,11 +13,13 @@ urlpatterns = [
     path(
         'auth/registration/', ProfileCreateView.as_view(), name='registration'
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.server_error'
